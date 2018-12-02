ee0a71a057ec2fb5e0cfc0828b7405afe83f2cdb
immigration9 <projgotham18@gmail.com>
2018-12-02 16:11:12 +0900

    [testing] jest added

---
+++ src/__test__/LineChart.test.js
@@ -0,0 +1,20 @@
+// import { LineChart, ChartMediator } from '../';
+
+/**
+ * Match Test
+ */
+
+test('null test', () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
+/**
+ * Async Test
+ */
+
+
